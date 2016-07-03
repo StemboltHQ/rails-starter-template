067f4bad786b0fc7d54a2c03687b4f71a365f086
Jared Norman <jared@creepywizard.com>
2016-07-02 23:20:08 -0700

    Clean up spec files, fix routes, other stuff

--- wizardry.rb
+++ wizardry.rb
@@ -1,6 +1,19 @@
+def install_template(path)
+  template_path = File.expand_path(File.join('..', path), __FILE__)
+  template = File.read(template_path)
+  file path, template, force: true
+end
+
+def commit_all(message)
+  git add: "."
+  git commit: "-m '#{message}'"
+end
+
+git :init
+
+# Disable generation of helpers, javascripts, css, and view specs.
 environment <<-RUBY
-# Disable generation of helpers, javascripts, css, and view specs
-    config.generators do |generate|
+config.generators do |generate|
       generate.helper false
       generate.assets false
       generate.view_specs false
@@ -10,13 +23,23 @@ RUBY
 gem_group :development, :test do
   gem "rspec-rails", "~> 3.5.0"
   gem "factory_girl_rails", "~> 4.7"
+  gem "capybara", "~> 2.7"
+  gem "poltergeist", "~> 1.10"
 end
 
 after_bundle do
   rails_command "db:create"
   rails_command "db:migrate"
+  commit_all "Initial commit"
 
   generate "rspec:install"
+  install_template "spec/spec_helper.rb"
+  install_template "spec/rails_helper.rb"
+
+  commit_all "Setup testing environment"
 
   generate "controller", "home", "index"
+  install_template "config/routes.rb"
+
+  commit_all "Add home controller"
 end
