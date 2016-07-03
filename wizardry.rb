1ee86959947e7e359cb2d173ee5e71d6bdccaea0
Jared Norman <jared@creepywizard.com>
2016-07-02 19:26:59 -0700

    Add a controller and rspec

--- wizardry.rb
+++ wizardry.rb
@@ -1,1 +1,14 @@
 # Nothing to see here yet.
+
+gem_group :development, :test do
+  gem "rspec-rails", "~> 3.5.0"
+end
+
+after_bundle do
+  rails_command "db:create"
+  rails_command "db:migrate"
+
+  generate "rspec:install"
+
+  generate "controller", "home", "index"
+end
